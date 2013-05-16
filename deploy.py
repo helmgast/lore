f1c356924e711e4544b8b365812bb5bd3cdf5865
RipperDoc <ripperdoc@gmail.com>
2013-05-17 00:25:11 +0900

    bugfix

--- deploy.py
+++ deploy.py
@@ -1,6 +1,7 @@
 import imp
 import os
 import sys
+from app_shared import app
 
 PYCART_DIR = ''.join(['python-', '.'.join(map(str, sys.version_info[:2]))])
 
@@ -22,12 +23,12 @@ def run_simple_httpd_server(app, ip, port=8080):
 def run():
     ip   = os.environ['OPENSHIFT_INTERNAL_IP']
     port = 8080
-    zapp = imp.load_source('application', 'wsgi/application')
+    #zapp = imp.load_source('application', 'wsgi/application')
 
     #  Use gevent if we have it, otherwise run a simple httpd server.
     print 'Starting WSGIServer on %s:%d ... ' % (ip, port)
     try:
-      run_gevent_server(zapp.application, ip, port)
+      run_gevent_server(app, ip, port)
     except:
       print 'gevent probably not installed - using default simple server ...'
-      run_simple_httpd_server(zapp.application, ip, port)
+      run_simple_httpd_server(app, ip, port)
