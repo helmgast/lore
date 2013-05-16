45ee2b2ab0ea327a0fec0c0983f0e117a842440d
RipperDoc <ripperdoc@gmail.com>
2013-05-17 08:46:45 +0900

    testing flask basic app first

--- app.py
+++ app.py
@@ -1,72 +1,46 @@
-import os, sys
+#!/usr/bin/env python
+import imp
+import os
+import sys
 
-import deploy
+PYCART_DIR = ''.join(['python-', '.'.join(map(str, sys.version_info[:2]))])
+
+try:
+   zvirtenv = os.path.join(os.environ['OPENSHIFT_HOMEDIR'], PYCART_DIR,
+                           'virtenv', 'bin', 'activate_this.py')
+   execfile(zvirtenv, dict(__file__ = zvirtenv) )
+except IOError:
+   pass
+
+
+def run_gevent_server(app, ip, port=8080):
+   from gevent.pywsgi import WSGIServer
+   WSGIServer((ip, port), app).serve_forever()
+
+
+def run_simple_httpd_server(app, ip, port=8080):
+   from wsgiref.simple_server import make_server
+   make_server(ip, port, app).serve_forever()
+
+
 #
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
 # 
 
-from flask import Flask, Markup
-from flask_peewee.auth import Auth
-from flask_peewee.admin import Admin
-from flask_peewee.db import Database
-from re import compile
-from flaskext.markdown import Markdown
 
-try:
-  import simplejson as json
-except ImportError:
-  import json
-
-from app_shared import app, db, auth
-from admin import create_admin
-from api import create_api
-from views import *
-
-Markdown(app)
-
-admin = create_admin(app, auth)
-api = create_api(app, auth)
-
-from world import world
-from social import social
-from generator import generator
-from campaign import campaign
-
-app.register_blueprint(world, url_prefix='/world')
-app.register_blueprint(generator, url_prefix='/generator')
-app.register_blueprint(social, url_prefix='/social')
-app.register_blueprint(campaign, url_prefix='/campaign')
-
-@app.template_filter('is_following')
-def is_following(from_user, to_user):
-    return from_user.is_following(to_user)
-    
-wikify_re = compile(r'\b(([A-Z]+[a-z]+){2,})\b')
-
-@app.template_filter('wikify')
-def wikify(s):
-    return Markup(wikify_re.sub(r'<a href="/world/\1/">\1</a>', s))
-
-@app.template_filter('dictreplace')
-def dictreplace(s, d):
-    #print "Replacing %s with %s" % (s,d)
-    if d and len(d) > 0:
-        parts = s.split("__")
-        # Looking for variables __key__ in s.
-        # Splitting on __ makes every 2nd part a key, starting with index 1 (not 0)
-        for i in range(1,len(parts),2):
-            parts[i] = d[parts[i]] # Replace with dict content
-        return ''.join(parts)
-    return s
-
+#
+#  main():
+#
 if __name__ == '__main__':
-  if True:#'OPENSHIFT_INTERNAL_IP' in os.environ:
-    deploy.run()
-  else:
-    print "Running local"
-    if len(sys.argv) > 1 and sys.argv[1] == "reset":
-      print "Resetting data models"
-      model_setup.setup_models()
-      exit()
-    app.run(debug=True) # Debug will reload code automatically, so no need to restart server
+   ip   = os.environ['OPENSHIFT_INTERNAL_IP']
+   port = 8080
+   zapp = imp.load_source('application', 'wsgi/application')
+
+   #  Use gevent if we have it, otherwise run a simple httpd server.
+   print 'Starting WSGIServer on %s:%d ... ' % (ip, port)
+   try:
+      run_gevent_server(zapp.application, ip, port)
+   except:
+      print 'gevent probably not installed - using default simple server ...'
+      run_simple_httpd_server(zapp.application, ip, port)
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='Raconteur', version='0.3',
 
       #  Uncomment one or more lines below in the install_requires section
       #  for the specific client drivers/modules your application needs.
-      install_requires=['greenlet', 'gevent',
+      install_requires=['greenlet', 'gevent', 'flask',
                         #  'MySQL-python',
                         #  'pymongo',
                         #  'psycopg2',
