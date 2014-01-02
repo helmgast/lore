dc908f75dd3f7c0ca152e26bd90c440552c9c967
Niklas Frojd <niklas.frojd@gmail.com>
2014-01-02 23:32:34 +0100

    New package for models

---
+++ model/misc.py
@@ -0,0 +1,40 @@
+from raconteur import db
+import re
+
+'''
+Created on 2 jan 2014
+
+@author: Niklas
+'''
+# WTForms would treat the _absence_ of a field in POST data as a reason to
+# to set the data to empty. This is a problem if the same POST receives variations
+# to a form. This method removes form fields if they are not present in postdata.
+# This means the form logic will not touch those fields in the actual objects.
+def matches_form(formclass, formdata):
+    for k in formdata.iterkeys():
+        if k in dir(formclass):
+            print "Matches field %s!" % k
+            return True
+    return False
+  
+def slugify(s):
+    return re.sub('[^a-z0-9_\-]+', '-', s.lower())
+
+class GeneratorInputList(db.Document):
+    name = db.StringField()
+
+    def items(self):
+        return GeneratorInputItem.select().where(GeneratorInputItem.input_list == self)
+
+class GeneratorInputItem(db.Document):
+    input_list = db.ReferenceField(GeneratorInputList)
+    content = db.StringField()
+
+class StringGenerator(db.Document):
+    name = db.StringField()
+    description = db.StringField()
+    generator = None
+
+    def __unicode__(self):
+        return self.name
+
