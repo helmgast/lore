ba2f55df6e11db55b23d5b4bcad85b78411fd2c8
Niklas Frojd <niklas.frojd@gmail.com>
2014-03-08 19:32:08 +0100

    Unfinished sample data for batch insert

---
+++ db/batch_insert.js
@@ -0,0 +1,25 @@
+/* Usage: mongo <db_name> */
+/* This is just a sample file */
+
+//db.article.insert( { _id: 10, type: "misc", item: "card", qty: 15 } )
+
+var sample_world = db.world.findOne();
+printjson(sample_world.title);
+
+
+var sample_data = {
+    "slug": "ljusbringaren-bild",
+    "type": 1,
+    "world": sample_world.id,
+    "creator": ObjectId("531b1385b7426f25a8f60d25"),
+    "created_date": ISODate(),
+    "title": "Ljusbringaren bild",
+    "content": "No content",
+    "status": 0,
+    "imagearticle": {
+        "image": ObjectId("531b1388b7426f25a8f60d33"),
+        "source_image_url": "http://kaigon.se/wiki/images/6/6b/Ljusets_son.jpg",
+        "mime_type": "image/jpeg"
+    },
+    "relations": [ ]
+};
