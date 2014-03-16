e0b1ba3ad418f386de150cbbf84abc5841f1101a
Niklas Frojd <niklas.frojd@gmail.com>
2014-03-16 12:26:06 +0100

    Tab to space

--- raconteur.py
+++ raconteur.py
@@ -1,11 +1,11 @@
 """
-	raconteur.raconteur
-	~~~~~~~~~~~~~~~~
+  raconteur.raconteur
+  ~~~~~~~~~~~~~~~~
 
-	Main raconteur application class, that initializes the Flask application,
-	it's blueprints, plugins and template filters.
+  Main raconteur application class, that initializes the Flask application,
+  it's blueprints, plugins and template filters.
 
-	:copyright: (c) 2014 by Raconteur
+  :copyright: (c) 2014 by Raconteur
 """
 
 import logging
@@ -28,25 +28,25 @@ from markdown.inlinepatterns import ImagePattern, IMAGE_LINK_RE
 from markdown.util import etree
 
 class MongoJSONEncoder(JSONEncoder):
-	def default(self, o):
-		if isinstance(o, Document) or isinstance(o, QuerySet):
-			return o.to_json()
-		return JSONEncoder.default(self, o)
+  def default(self, o):
+    if isinstance(o, Document) or isinstance(o, QuerySet):
+      return o.to_json()
+    return JSONEncoder.default(self, o)
 
 class NewImagePattern(ImagePattern):
-	def handleMatch(self, m):
-		el = super(NewImagePattern, self).handleMatch(m)
-		alt = el.get('alt')
-		src = el.get('src')
-		parts = alt.rsplit('|',1)
-		if len(parts)==2:
-			el.set('alt',parts[0])
-			el.set('class', parts[1])
-		el.set('src', src.replace('/images/','/images/thumbs/'))
-		a_el = etree.Element('a')
-		a_el.append(el)
-		a_el.set('href', src)
-		return a_el
+  def handleMatch(self, m):
+    el = super(NewImagePattern, self).handleMatch(m)
+    alt = el.get('alt')
+    src = el.get('src')
+    parts = alt.rsplit('|',1)
+    if len(parts)==2:
+      el.set('alt',parts[0])
+      el.set('class', parts[1])
+    el.set('src', src.replace('/images/','/images/thumbs/'))
+    a_el = etree.Element('a')
+    a_el.append(el)
+    a_el.set('href', src)
+    return a_el
 
 class AutolinkedImage(Extension):
     def extendMarkdown(self, md, md_globals):
@@ -65,107 +65,107 @@ app_state = STATE_PUBLIC
 
 
 def is_private():
-	return app_state == STATE_PRIVATE
+  return app_state == STATE_PRIVATE
 
 
 def is_protected():
-	return app_state == STATE_PROTECTED
+  return app_state == STATE_PROTECTED
 
 
 def is_public():
-	return app_state == STATE_PUBLIC
+  return app_state == STATE_PUBLIC
 
 
 def is_allowed_access(user):
-	if is_private():
-		return False
-	elif is_protected():
-		return user.admin if user else False
-	else:
-		return True
+  if is_private():
+    return False
+  elif is_protected():
+    return user.admin if user else False
+  else:
+    return True
 
 if the_app is None:
-	from app import is_debug
+  from app import is_debug
 
-	the_app = Flask('raconteur')  # Creates new flask instance
-	logger = logging.getLogger(__name__)
-	logger.info("App created: %s", the_app)
-	the_app.config.from_pyfile('config.py')  # db-settings and secrets, should not be shown in code
-	the_app.config['DEBUG'] = is_debug
-	the_app.config['PROPAGATE_EXCEPTIONS'] = is_debug
-	the_app.json_encoder = MongoJSONEncoder
-	db = MongoEngine(the_app)  # Initiate the MongoEngine DB layer
-	# we can't import models before db is created, as the model classes are built on runtime knowledge of db
+  the_app = Flask('raconteur')  # Creates new flask instance
+  logger = logging.getLogger(__name__)
+  logger.info("App created: %s", the_app)
+  the_app.config.from_pyfile('config.py')  # db-settings and secrets, should not be shown in code
+  the_app.config['DEBUG'] = is_debug
+  the_app.config['PROPAGATE_EXCEPTIONS'] = is_debug
+  the_app.json_encoder = MongoJSONEncoder
+  db = MongoEngine(the_app)  # Initiate the MongoEngine DB layer
+  # we can't import models before db is created, as the model classes are built on runtime knowledge of db
 
-	from model.user import User
+  from model.user import User
 
-	auth = Auth(the_app, db, user_model=User)
-#	admin = Admin(the_app, auth)
+  auth = Auth(the_app, db, user_model=User)
+#  admin = Admin(the_app, auth)
 
-	md = Markdown(the_app)
-	md.register_extension(AutolinkedImage)
-	csrf = CsrfProtect(the_app)
-	babel = Babel(the_app)
+  md = Markdown(the_app)
+  md.register_extension(AutolinkedImage)
+  csrf = CsrfProtect(the_app)
+  babel = Babel(the_app)
 
-	from controller.world import world_app as world
-	from controller.social import social
-	from controller.generator import generator
-	from controller.campaign import campaign_app as campaign
-	from resource import ResourceError
+  from controller.world import world_app as world
+  from controller.social import social
+  from controller.generator import generator
+  from controller.campaign import campaign_app as campaign
+  from resource import ResourceError
 
-	the_app.register_blueprint(world, url_prefix='/world')
-	the_app.register_blueprint(generator, url_prefix='/generator')
-	the_app.register_blueprint(social, url_prefix='/social')
-	the_app.register_blueprint(campaign, url_prefix='/campaign')
+  the_app.register_blueprint(world, url_prefix='/world')
+  the_app.register_blueprint(generator, url_prefix='/generator')
+  the_app.register_blueprint(social, url_prefix='/social')
+  the_app.register_blueprint(campaign, url_prefix='/campaign')
 
 
 def run_the_app(debug):
-	logger.info("Running local instance")
-	the_app.run(debug=debug)
+  logger.info("Running local instance")
+  the_app.run(debug=debug)
 
 
 def setup_models():
-	logger.info("Resetting data models")
-	db.connection.drop_database(the_app.config['MONGODB_SETTINGS']['DB'])
-	from test_data import model_setup
-	model_setup.setup_models()
-	# This hack sets a unique index on the md5 of image files to prevent us from 
-	# uploading duplicate images
-	# db.connection[the_app.config['MONGODB_SETTINGS']['DB']]['images.files'].ensure_index(
+  logger.info("Resetting data models")
+  db.connection.drop_database(the_app.config['MONGODB_SETTINGS']['DB'])
+  from test_data import model_setup
+  model_setup.setup_models()
+  # This hack sets a unique index on the md5 of image files to prevent us from 
+  # uploading duplicate images
+  # db.connection[the_app.config['MONGODB_SETTINGS']['DB']]['images.files'].ensure_index(
  #        'md5', unique=True, background=True)
 
 def validate_model():
-	is_ok = True
-	pkgs = ['model.campaign', 'model.misc', 'model.user', 'model.world']  # Look for model classes in these packages
-	for doc in db.Document._subclasses:  # Ugly way of finding all document type
-		if doc != 'Document':  # Ignore base type (since we don't own it)
-			for pkg in pkgs:
-				try:
-					cls = getattr(__import__(pkg, fromlist=[doc]), doc)  # Do add-hoc import/lookup of type, simillar to from 'pkg' import 'doc'
-					try:
-						cls.objects()  # Check all objects of type
-					except TypeError:
-						logger.error("Failed to instantiate %s", cls)
-						is_ok = False
-				except AttributeError:
-					pass  # Ignore errors from getattr
-				except ImportError:
-					pass  # Ignore errors from __import__
-	logger.info("Model has been validated" if is_ok else "Model has errors, aborting startup")
-	return is_ok
+  is_ok = True
+  pkgs = ['model.campaign', 'model.misc', 'model.user', 'model.world']  # Look for model classes in these packages
+  for doc in db.Document._subclasses:  # Ugly way of finding all document type
+    if doc != 'Document':  # Ignore base type (since we don't own it)
+      for pkg in pkgs:
+        try:
+          cls = getattr(__import__(pkg, fromlist=[doc]), doc)  # Do add-hoc import/lookup of type, simillar to from 'pkg' import 'doc'
+          try:
+            cls.objects()  # Check all objects of type
+          except TypeError:
+            logger.error("Failed to instantiate %s", cls)
+            is_ok = False
+        except AttributeError:
+          pass  # Ignore errors from getattr
+        except ImportError:
+          pass  # Ignore errors from __import__
+  logger.info("Model has been validated" if is_ok else "Model has errors, aborting startup")
+  return is_ok
 
 
 def run_tests():
-	logger.info("Running unit tests")
-	from tests import app_test
-	app_test.run_tests()
+  logger.info("Running unit tests")
+  from tests import app_test
+  app_test.run_tests()
 
 if not is_debug:
-	@the_app.errorhandler(ResourceError)
-	def handle_invalid_usage(error):
-			response = jsonify(error.to_dict())
-			print request.args
-			return response
+  @the_app.errorhandler(ResourceError)
+  def handle_invalid_usage(error):
+      response = jsonify(error.to_dict())
+      print request.args
+      return response
 
 
 ###
@@ -173,15 +173,15 @@ if not is_debug:
 ###
 @the_app.route('/')
 def homepage():
-	return render_template('homepage.html')
+  return render_template('homepage.html')
 
 
 @auth.admin_required
 @the_app.route('/admin/', methods=['GET', 'POST'])
 def admin():
-	if request.method == 'POST' and request.form['state']:
-		pass
-	return render_template('maintenance.html')
+  if request.method == 'POST' and request.form['state']:
+    pass
+  return render_template('maintenance.html')
 
 
 JoinForm = model_form(User)
@@ -191,23 +191,23 @@ JoinForm = model_form(User)
 @the_app.route('/join/', methods=['GET', 'POST'])
 def join():
 
-		if request.method == 'POST' and request.form['username']:
-				# Read username from the form that was posted in the POST request
-				try:
-						User.objects().get(username=request.form['username'])
-						flash(_('That username is already taken'))
-				except User.DoesNotExist:
-						user = User(
-								username=request.form['username'],
-								email=request.form['email'],
-						)
-						user.set_password(request.form['password'])
-						user.save()
+    if request.method == 'POST' and request.form['username']:
+        # Read username from the form that was posted in the POST request
+        try:
+            User.objects().get(username=request.form['username'])
+            flash(_('That username is already taken'))
+        except User.DoesNotExist:
+            user = User(
+                username=request.form['username'],
+                email=request.form['email'],
+            )
+            user.set_password(request.form['password'])
+            user.save()
 
-						auth.login_user(user)
-						return redirect(url_for('homepage'))
-		join_form = JoinForm()
-		return render_template('join.html', join_form=join_form)
+            auth.login_user(user)
+            return redirect(url_for('homepage'))
+    join_form = JoinForm()
+    return render_template('join.html', join_form=join_form)
 
 
 ###
@@ -215,32 +215,32 @@ def join():
 ###
 @the_app.template_filter('is_following')
 def is_following(from_user, to_user):
-		return from_user.is_following(to_user)
+    return from_user.is_following(to_user)
 
 wikify_re = compile(r'\b(([A-Z]+[a-z]+){2,})\b')
 
 
 @the_app.template_filter('wikify')
 def wikify(s):
-		if s:
-			return Markup(wikify_re.sub(r'<a href="/world/\1/">\1</a>', s))
-		else:
-			return ""
+    if s:
+      return Markup(wikify_re.sub(r'<a href="/world/\1/">\1</a>', s))
+    else:
+      return ""
 
 
 @the_app.template_filter('dictreplace')
 def dictreplace(s, d):
-		if d and len(d) > 0:
-				parts = s.split("__")
-				# Looking for variables __key__ in s.
-				# Splitting on __ makes every 2nd part a key, starting with index 1 (not 0)
-				for i in range(1, len(parts), 2):
-						parts[i] = d[parts[i]]  # Replace with dict content
-				return ''.join(parts)
-		return s
+    if d and len(d) > 0:
+        parts = s.split("__")
+        # Looking for variables __key__ in s.
+        # Splitting on __ makes every 2nd part a key, starting with index 1 (not 0)
+        for i in range(1, len(parts), 2):
+            parts[i] = d[parts[i]]  # Replace with dict content
+        return ''.join(parts)
+    return s
 
 
 # i18n
 @babel.localeselector
 def get_locale():
-		return "sv"  # request.accept_languages.best_match(LANGUAGES.keys()) # Add 'sv' here instead to force swedish translation.
+    return "sv"  # request.accept_languages.best_match(LANGUAGES.keys()) # Add 'sv' here instead to force swedish translation.
