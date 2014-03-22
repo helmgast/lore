4a08cfc9ddb07775517ef6953a16dfe48908ada8
RipperDoc <ripperdoc@gmail.com>
2014-03-23 00:03:39 +0900

    Reinclude config.py to be tracked as it only contains dummy values. On production will use environ instead.

---
+++ config.py
@@ -0,0 +1,9 @@
+# Default config file, will use other in production
+MONGODB_SETTINGS = {'DB':'raconteurdb'}
+SECRET_KEY = 'raconteur'
+DEBUG = True
+
+LANGUAGES = {
+    'en': 'English',
+    'sv': 'Swedish'
+}
