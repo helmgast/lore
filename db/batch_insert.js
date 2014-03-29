1b81387a36b3c241ea9a7a6f1b2be028ec643fbf
Niklas Frojd <niklas.frojd@gmail.com>
2014-03-29 17:09:38 +0100

    Added init-data import files

--- db/batch_insert.js
+++ db/batch_insert.js
@@ -1,25 +1,2 @@
-/* Usage: mongo <db_name> */
-/* This is just a sample file */
-
-//db.article.insert( { _id: 10, type: "misc", item: "card", qty: 15 } )
-
-var sample_world = db.world.findOne();
-printjson(sample_world.title);
-
-
-var sample_data = {
-    "slug": "ljusbringaren-bild",
-    "type": 1,
-    "world": sample_world.id,
-    "creator": ObjectId("531b1385b7426f25a8f60d25"),
-    "created_date": ISODate(),
-    "title": "Ljusbringaren bild",
-    "content": "No content",
-    "status": 0,
-    "imagedata": {
-        "image": ObjectId("531b1388b7426f25a8f60d33"),
-        "source_image_url": "http://kaigon.se/wiki/images/6/6b/Ljusets_son.jpg",
-        "mime_type": "image/jpeg"
-    },
-    "relations": [ ]
-};
+/* Usage: mongoimport.exe --db <db_name> --collection world --file fablr-world.js */
+/* Usage: mongoimport.exe --db <db_name> --collection user --file fablr-user.js */
---
+++ db/fablr-user.js
@@ -0,0 +1,2 @@
+﻿{ username: "Martin", realname: "Martin F", email: "martin@helmgast.se", admin: true, password: "0c6ea$a65b545d88bda502d5802fa33257dae007e9be38" }
+{ username: "Niklas", realname: "Niklas F", email: "niklas@helmgast.se", admin: true, password: "72b24$519c6f54dd143e25f894d9cd926756e782ca2d18" }
