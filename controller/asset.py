040af393a73587d9a3156f63e9162d714c768a56
Niklas Frojd <niklas.frojd@gmail.com>
2015-04-11 16:43:26 +0200

    #109: File assets return to list after edit/new/replace

--- controller/asset.py
+++ controller/asset.py
@@ -10,7 +10,7 @@ logger = current_app.logger if current_app else logging.getLogger(__name__)
 
 asset_app = Blueprint('assets', __name__, template_folder='../templates/asset')
 
-file_asset_strategy = ResourceRoutingStrategy(FileAsset, 'files', 'slug', short_url=True)
+file_asset_strategy = ResourceRoutingStrategy(FileAsset, 'files', 'slug', short_url=True, post_edit_action='list')
 ResourceHandler.register_urls(asset_app, file_asset_strategy)
 
 
