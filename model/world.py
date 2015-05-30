65e531aefcbe819706a1cf64c24ba74459c0b93d
Niklas Frojd <niklas.frojd@gmail.com>
2015-05-30 14:07:38 +0200

    Removing EmbeddedDocumentListField

--- model/world.py
+++ model/world.py
@@ -177,8 +177,8 @@ class Episode(db.EmbeddedDocument):
 # may not have any effect
 
 class CampaignData(db.EmbeddedDocument):
-  pass # TODO, the children her and above gives DuplicateIndices errors. Need to be fixed.
-  children = db.EmbeddedDocumentListField(Episode)
+#  pass # TODO, the children her and above gives DuplicateIndices errors. Need to be fixed.
+#  children = db.EmbeddedDocumentListField(Episode)
 
 # class Tree(db.EmbeddedDocument):
 #   pass
